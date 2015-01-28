72b066c4a74c472199ab84832977ae4302bbf647
antoineclaval <antoine.claval@gmail.com>
2015-01-28 18:02:10 +0100

    settings and model description for previous commit

--- coopInfo/models.py
+++ coopInfo/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
+class State(models.Model):
+    name = models.CharField(max_length=15)
+
+    def __unicode__(self):
+        return self.name
+
 
 class Cooperative(models.Model):
+    stateId = models.ForeignKey(State)
     name = models.CharField(max_length=50)
     acronym = models.CharField(max_length=6)
-    address = models.CharField(max_length=50)
+    streetAddress = models.CharField(max_length=50)
     website = models.CharField(max_length=255, unique=True)
-    phone = models.CharField(max_length=50)
+    mailAddress = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    countiesServed =  models.CharField(max_length=500)
     nextElectionTerms = models.CharField(max_length=50)
     servingTime = models.CharField(max_length=50)
     annualMeeting = models.CharField(max_length=20)
@@ -25,7 +34,7 @@ class Cooperative(models.Model):
 class Person(models.Model):
     coopId = models.ForeignKey(Cooperative)
     name = models.CharField(max_length=20)
-    etnicity = models.CharField(max_length=10)
+    ethnicity = models.CharField(max_length=10)
     distric = models.CharField(max_length=255)
     title = models.CharField(max_length=20)
     
