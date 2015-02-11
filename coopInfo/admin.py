599fd1e57d7a3ec0e2003a39706dd86ecabce65c
antoineclaval <antoine.claval@gmail.com>
2015-02-11 11:29:23 +0100

    add conveniance fields and colums to admin panel

--- coopInfo/admin.py
+++ coopInfo/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from coopInfo.models import Cooperative , Person , State
 
+class CooperativeAdmin(admin.ModelAdmin):
+	list_display = ('name', 'stateId','website' )
+	search_fields = ['name']
+	list_filter = ['stateId']
+	fieldsets = [
+	    ('Basic info', {'fields': ['name', 'website', 'acronym']}),
+	    ('Mail Info', {'fields': ['streetAddress', 'mailAddress']}),
+	    ('Contact', {'fields': ['email', 'phone']}),
+	    ('Administrative', {'fields':['bylaws','servingTime','nextElectionTerms','annualMeeting','montlyMeeting']}),
+	    ('Misc', {'fields': ['consumers','numberOfEmployees','milesOfLines','countiesServed']})
+	    ]
+
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ('name', 'ethnicity', 'coopId','title')
+	search_fields = ['name']
+	list_filter = ['ethnicity', 'title']
+
 # Register your models here.
-admin.site.register(Cooperative)
-admin.site.register(Person)
+admin.site.register(Cooperative, CooperativeAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(State)
