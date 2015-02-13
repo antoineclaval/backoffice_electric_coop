fe48a371b7b2db66e3c2d281f2108102cdf005cb
antoineclaval <antoine.claval@gmail.com>
2015-02-13 15:26:15 +0100

    Allow blank value in django admin form. Customise Django Admin title. Add edition of persons in Coops Admin form

--- coopInfo/admin.py
+++ coopInfo/admin.py
@@ -1,22 +1,32 @@
 from django.contrib import admin
 from coopInfo.models import Cooperative , Person , State
 
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ('name', 'ethnicity', 'coopId','title')
+	search_fields = ['name']
+	list_filter = ['ethnicity', 'title']
+
+class PersonInline(admin.TabularInline):
+    model = Person
+    extra = 8
+
 class CooperativeAdmin(admin.ModelAdmin):
 	list_display = ('name', 'stateId','website' )
 	search_fields = ['name']
 	list_filter = ['stateId']
+	inlines = [PersonInline]
 	fieldsets = [
 	    ('Basic info', {'fields': ['name', 'website', 'acronym']}),
 	    ('Mail Info', {'fields': ['streetAddress', 'mailAddress']}),
 	    ('Contact', {'fields': ['email', 'phone']}),
 	    ('Administrative', {'fields':['bylaws','servingTime','nextElectionTerms','annualMeeting','montlyMeeting']}),
-	    ('Misc', {'fields': ['consumers','numberOfEmployees','milesOfLines','countiesServed']})
+	    ('Misc', {'fields': ['consumers','numberOfEmployees','milesOfLines','countiesServed']}),
+	    ('Documents', {'fields': ['bylaws', 'is990present']})
 	    ]
 
-class PersonAdmin(admin.ModelAdmin):
-	list_display = ('name', 'ethnicity', 'coopId','title')
-	search_fields = ['name']
-	list_filter = ['ethnicity', 'title']
+#Customise Admin site 
+admin.site.site_header = 'Electric Coop Backoffice'
+admin.site.site_title = 'Electric Coop Backoffice'
 
 # Register your models here.
 admin.site.register(Cooperative, CooperativeAdmin)
--- coopInfo/models.py
+++ coopInfo/models.py
@@ -10,22 +10,22 @@ class State(models.Model):
 class Cooperative(models.Model):
     stateId = models.ForeignKey(State)
     name = models.CharField(max_length=50)
-    acronym = models.CharField(max_length=6)
-    streetAddress = models.CharField(max_length=150, verbose_name='street address')
-    website = models.CharField(max_length=255, unique=True)
-    mailAddress = models.CharField(max_length=50, verbose_name='mail address')
-    email = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
-    countiesServed =  models.CharField(max_length=500, verbose_name='counties served')
-    consumers = models.IntegerField(default=0, null=True, verbose_name='number of consumers')
-    montlyMeeting = models.CharField(max_length=20, verbose_name='info about montly meeting')
-    annualMeeting = models.CharField(max_length=20, verbose_name='info about annual meeting')
-    numberOfEmployees = models.IntegerField(default=0, null=True, verbose_name='number of employees')
-    milesOfLines = models.IntegerField(default=0, null=True, verbose_name='miles of electric lines')
-    nextElectionTerms = models.CharField(max_length=50, verbose_name='next election')
-    servingTime = models.CharField(max_length=50, verbose_name='serving time')
-    bylaws = models.CharField(max_length=255)
-    is990present = models.BooleanField(default=False)
+    acronym = models.CharField(max_length=6, blank=True)
+    streetAddress = models.CharField(max_length=150, verbose_name='street address', blank=True)
+    website = models.CharField(max_length=255, unique=True, blank=True)
+    mailAddress = models.CharField(max_length=50, verbose_name='mail address', blank=True)
+    email = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
+    countiesServed =  models.CharField(max_length=500, verbose_name='counties served', blank=True)
+    consumers = models.IntegerField(default=0, null=True, verbose_name='number of consumers', blank=True)
+    montlyMeeting = models.CharField(max_length=500, verbose_name='info about montly meeting', blank=True)
+    annualMeeting = models.CharField(max_length=500, verbose_name='info about annual meeting', blank=True)
+    numberOfEmployees = models.IntegerField(default=0, null=True, verbose_name='number of employees', blank=True)
+    milesOfLines = models.IntegerField(default=0, null=True, verbose_name='miles of electric lines', blank=True)
+    nextElectionTerms = models.CharField(max_length=50, verbose_name='next election', blank=True)
+    servingTime = models.CharField(max_length=50, verbose_name='serving time', blank=True)
+    bylaws = models.CharField(max_length=255, blank=True)
+    is990present = models.BooleanField(default=False, blank=True)
 
     def __unicode__(self):
         return self.name
@@ -34,10 +34,10 @@ class Cooperative(models.Model):
 class Person(models.Model):
     coopId = models.ForeignKey(Cooperative)
     name = models.CharField(max_length=20)
-    ethnicity = models.CharField(max_length=10)
-    distric = models.CharField(max_length=255)
-    title = models.CharField(max_length=20)
-    inBoardSince = models.DateTimeField(null=True, verbose_name='in board since')
+    ethnicity = models.CharField(max_length=10, blank=True)
+    distric = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=20, blank=True)
+    inBoardSince = models.DateTimeField(null=True, verbose_name='in board since', blank=True)
 
     def __unicode__(self):
         return self.name
