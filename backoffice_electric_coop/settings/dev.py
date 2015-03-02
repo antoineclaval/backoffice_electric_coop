8164bd245e788226ad31b6f9ab14bee432faf226
antoineclaval <antoine.claval@gmail.com>
2015-03-02 18:08:01 +0100

    add person pictures. Add dev settings for servig them. Change path of person to persons

--- backoffice_electric_coop/settings/dev.py
+++ backoffice_electric_coop/settings/dev.py
@@ -4,3 +4,6 @@ from .base import *
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+# why : i'm serving media ( and static ) from python -m SimpleHTTPServer 8001 in BASE_DIR
+MEDIA_URL = "http://localhost:8001/media/"
